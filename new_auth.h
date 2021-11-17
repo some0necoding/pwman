4c60cf62cc898658bbcfb953abfd651ddcd5d487
some0necoding <93733771+some0necoding@users.noreply.github.com>
2021-11-17 07:44:53 +0100

    upgrading authentication file

--- new_auth.h
+++ new_auth.h
@@ -1,7 +1,7 @@
 #include <string.h>
 
-#ifndef ECHOCTL
-    #include <termios.h>
+#ifndef TERMIOS_PLUS_PLUS
+    #include <termiosplusplus.h>
 #endif
 
 #ifndef STDIO_PLUS_PLUS
@@ -155,25 +155,6 @@ int psm_read_pass(char *buffer)
     enable_terminal_echo(old);
 
     return 1;
-}
-
-struct termios disable_terminal_echo() {
-
-    struct termios old_t;
-    struct termios new_t;
-
-    (void) tcgetattr(fileno(stdin), &old_t);
-
-    new_t = old_t;
-    new_t.c_lflag &= ~ECHO;
-
-    (void) tcsetattr(fileno(stdin), TCSAFLUSH, &new_t);
-
-    return old_t;
-}
-
-void enable_terminal_echo(struct termios old_t) {
-    (void) tcsetattr(fileno(stdin), TCSAFLUSH, &old_t);
 }
 
 int auth_pass(char *password) 
