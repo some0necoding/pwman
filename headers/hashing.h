80f2717d3dd590ba6beb2bab79a93d1cfbe09ef7
some0necoding <93733771+some0necoding@users.noreply.github.com>
2021-11-21 14:23:34 +0100

    making some headers

---
+++ headers/hashing.h
@@ -0,0 +1,13 @@
+#ifndef HASHING
+#define HASHING
+
+// hashes a password "pass" of "pass_len" length
+char *pass_hash(char *pass, size_t pass_len);
+
+// stores a hash "hash" in a file stored at "file_path" path
+int store_hash(char *hash, char *file_path);
+
+// retrieves a hash "hash" in a file stored at "file_path" path
+int get_hash(char *hash, char *file_path);
+
+#endif
