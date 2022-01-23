740a7974ef735a06941f04e9b2837bba82e97698
some0necoding <codingandprogramming.code@gmail.com>
2022-01-23 17:33:01 +0100

    trying to fix logging (but not yet fixed)

--- headers/logging.h
+++ headers/logging.h
@@ -2,6 +2,6 @@
 #define LOGGER_H
 
 // just some logger with built-in message formatting
-void logger(const char *log_file_path, const char *tag, const char *message, ...);
+int logger(const char *log_file_path, const char *tag, const char *message, ...);
 
 #endif
