5b1b42a73503dceae3d08f753b320582da76234b
some0necoding <codingandprogramming.code@gmail.com>
2022-02-20 16:46:38 +0100

    adding help command

--- passwordmanager.c
+++ passwordmanager.c
@@ -38,6 +38,7 @@ int psm_add(char ** args);
 int psm_modify(char **args);
 int psm_remove(char **args);
 int psm_get(char **args);
+int psm_help(char **args);
 int psm_exit(char **args);
 
 char **psm_split_line(char *line);
@@ -108,6 +109,7 @@ char *command_names[] = {
     "edit",
     "rm",
     "get",
+    "help",
     "exit"
 };
 
@@ -118,6 +120,7 @@ int (*command_addr[]) (char **) = {
     &psm_modify,
     &psm_remove,
     &psm_get,
+    &psm_help,
     &psm_exit
 };
 
@@ -327,7 +330,59 @@ int psm_get(char **args)
     return 0;
 }
 
-int psm_exit(char **args) {
+int psm_help(char **args)
+{
+    unsigned char *help_txt;
+
+    if (args[1]) {
+        printf("\"help\" does not accept arguments\n");
+        return -1;
+    }
+
+    help_txt = "\nWHAT IS PASSMAN:\n"
+               "\n"
+               "    Passman stores and retrieves accounts along with\n"
+	           "    their passwords.\n"
+               "\n"
+	           "    Every account is made up by three parts:\n"
+		       "        - account name\n" 
+		       "        - email or username used to log in the account\n"
+		       "        - password used to log in the account\n"
+               "\n"
+               "SYNTAX:\n" 
+               "\n"
+	           "    show\n" 
+	           "    add <account-name> <user-or-mail>\n" 
+	           "    rm <account-name>\n"
+	           "    get <account-name>\n" 
+	           "    help\n"
+               "\n"
+               "DESCRIPTION:\n"
+               "\n"
+	           "    show\n" 
+		       "        To show all accounts stored.\n"
+               "\n"
+	           "    add\n" 
+		       "        To add a new account.\n"
+               "\n"
+	           "    rm\n"
+		       "        To remove an account.\n" 
+               "\n"
+	           "    get\n"
+		       "        To retrieve a password. Note that it will be saved\n"
+               "        into the (X11) clipboard so that it can be directly\n"
+               "        pasted.\n"
+               "\n"
+	           "    help\n" 
+		       "        To display help page.\n";
+
+    printf("%s\n", help_txt);
+
+    return 0;
+}
+
+int psm_exit(char **args) 
+{
     exit(EXIT_SUCCESS);
 }
 
