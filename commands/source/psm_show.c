b8514d0751d07cd480a01ec51d43f1300180fd85
some0necoding <codingandprogramming.code@gmail.com>
2022-11-05 17:05:41 +0100

    show command finally working; fixed memory leaks

--- commands/source/psm_show.c
+++ commands/source/psm_show.c
@@ -1,44 +1,20 @@
 #include "../../utils/headers/config.h"
 #include "../../utils/headers/array_handling.h"
 
+#define _XOPEN_SOURCE 500
+
 #include <stdio.h>
 #include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
 #include <stdlib.h>
-
-/*----------CONSTANTS-DEFINITION-START----------*/
-
-char *PATH;         // local variable PATH
-
-/*-----------CONSTANTS-DEFINITION-END-----------*/
-
-/*------------GLOBAL-VARIABLES-START------------*/
-
-// every file has a path, a name and 
-// an indentation level.
-typedef struct {
-    char *path;
-    char *name;
-    int indentation;
-} file_name;
-
-/*-------------GLOBAL-VARIABLES-END-------------*/
+#include <sys/stat.h>
+#include <ftw.h>
 
 /*----------FUNCTIONS-DEFINITION-START----------*/
 
-int list(char *path);
-file_name *get_subdirs(char *path, int *subdirs_qty);
-int list_rec(char *path);
-int list_file(char *path);
-int is_dir(char *path);
-int is_file(char *path);
-char *build_path(char *root, char *rel_path);
-char *get_last_dir(char *path);
-int print_file(char *name, int indentation);
-int get_indentation_level(char *sub_path);
-int get_tok_num(char *path); 
-char *remove_ext(char *fname, char *ext); 
+int build_path(char **root, char *rel_path);
+int print_file(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
+char *get_last_dir(const char *path);
+char *remove_ext(const char *fname, const char *ext); 
 
 /*-----------FUNCTIONS-DEFINITION-END-----------*/
 
@@ -51,292 +27,95 @@ char *remove_ext(char *fname, char *ext);
 */ 
 int psm_show(char **args)
 {
-    char *path = get_env_var("PATH");          // /home/{user}/.pwstore     
+    char *path = get_env_var("PATH");               // /home/{user}/.pwstore     
+    
+    if (check_allocation(path)) return -1;
 
-    if (!path) return -1;
-
-    if (args[1]) {
-        path = build_path(path, args[1]);      // /home/{user}/.pwstore/{args[1]}
+    if (args[1] && (build_path(&path, args[1]) != 0)) {      // /home/{user}/.pwstore/{args[1]}
+        perror("psm: ");
     }
 
-    // environment path is stored inside PATH variable
-    // with additional subdirectories if provided by user 
-    size_t path_len = strlen(path);
-    PATH = malloc(sizeof(char) * (path_len + 1));
-    strcpy(PATH, path); 
-
-    // listing subdirectories and files under PATH 
-    list(PATH);
+    if (nftw(path, print_file, 20, 0) == -1) {
+        perror("psm: I/O error");
+        return -1;
+    }
 
     free(path);
-    free(PATH);
     return 0;
-}
-
-/*
-    This function checks if the user is trying to show a file (in this 
-    case it is just printed) or a directory (in this case it is scanned
-    for subdirs and other files).
-*/
-int list(char *path) 
-{
-    // the last dir in the PATH is the root of the tree
-    char *root = get_last_dir(path);
-    printf("%s\n", root);
-
-    if (is_dir(path) == 0) {            // dirs are scanned for subdirs and files
-        list_rec(path);           
-    } else if (is_file(path) == 0) {    // files are just printed
-        print_file(path, 1);
-    } else {                            // otherwise path does not exist
-        printf("%s is not in the password store\n", path);
-    }
-
-    free(root);
-    return 0;
-}
-
-/*
-    This function recursively scans directories for subdirectories and
-    files
-*/
-int list_rec(char *path) 
-{
-    int subdirs_qty;
-    file_name *subdirs = get_subdirs(path, &subdirs_qty);
-
-    for (int i=0; i < subdirs_qty; i++) {
-
-        file_name subdir = subdirs[i];
-
-        if (is_file(subdir.path) == 0) {
-            print_file(subdir.name, subdir.indentation);
-        } else if (is_dir(subdir.path) == 0) {
-            print_file(subdir.name, subdir.indentation);
-            list_rec(subdir.path);
-        }
-    }
-
-    for (int i = 0; i < subdirs_qty; i++) {
-        file_name subdir = subdirs[i];
-        free(subdir.path);
-    }
-
-    free(subdirs);
-
-    return 0;
-}
-
-/*
-    This function returns an array of file_name structs 
-    representing all path subdirectories and file 
-*/
-file_name *get_subdirs(char *path, int *subdirs_qty) 
-{
-    DIR *dir = opendir(path);
-    struct dirent *direntp;
-
-    // creating subdirs array
-    size_t subdirs_len = 1;
-    file_name *subdirs = malloc(sizeof(file_name) * subdirs_len);
-    int pos = 0;
-
-    // checking for good opening
-    if (!dir) {
-        perror("psm: I/O error");
-        return NULL;
-    }
-
-    // for every subdir create a struct and add it
-    // to subdirs array
-    while ((direntp = readdir(dir)) != NULL) {
-
-        char first_char = direntp->d_name[0];
-
-        if (first_char != '.') {    // skipping dot files
-
-            // if the array is too small it gets stretched
-            if (pos >= subdirs_len) {
-                subdirs_len += 1;
-                subdirs = realloc(subdirs, sizeof(*subdirs) * subdirs_len);
-            }
-
-            file_name subdir;
-            subdir.name = direntp->d_name;                      // file or subdir name
-            subdir.path = build_path(path, subdir.name);        // file or subdir path
-
-            int indentation_level = get_indentation_level(subdir.path);
-
-            subdir.indentation = indentation_level;             // indentation level for formatting
-
-            subdirs[pos] = subdir;
-            pos++;
-        }
-    }
-
-    // returning also array size
-    *subdirs_qty = pos;
-    return subdirs;
-}
-
-/*
-    This function returns the indentation level of the subdir
-    based on the distance between tree's root and subdir
-
-    path 1: /foo/boo/loo  -> 3
-    path 2: /foo/boo      -> 2
-
-    distance = 3 - 2 = 1
-
-    print_line() output:
-
-    boo
-     |--loo
-    ____ <- distance = 1
-*/
-int get_indentation_level(char *sub_path) 
-{
-    int path_dir_num = get_tok_num(PATH);
-    int sub_path_dir_num = get_tok_num(sub_path);
-
-    return sub_path_dir_num - path_dir_num;
-}
-
-/*
-    This function returns the number of tokens contained 
-    in path (i.e. the number of / excluding the trailing 
-    one if it exists)
-
-    Example 1: /foo/boo  -> 2
-    Example 2: /foo/boo/ -> 2
-                       ^
-                this is ignored
-*/
-int get_tok_num(char *path) 
-{
-    int path_len = strlen(path);
-    int tok_num = 0;
-
-    for (int i = 0; i < path_len; i++) {
-        if (path[i] == '/' && path[i + 1] != '\0') {
-            tok_num++;
-        }
-    }
-
-    return tok_num;
 }
 
 /*
     This function prints a file name with the right indentation
 */
-int print_file(char *name, int indentation) 
+int print_file(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
-    name = remove_ext(name, ".gpg");
+    const char *last_dir = get_last_dir(path);
+    const char *FMT_TOK = "|--";
+    const char *name = remove_ext(last_dir, ".gpg");
 
-    if (check_allocation(name)) return -1;
-
-    int name_len = strlen(name);
-    int fmt_name_len = name_len + (indentation * 4);
-
-    char *fmt_name = malloc(fmt_name_len + 1);
-    char *fmt_tok = "|--";
-
-    for (int i = 0; i < fmt_name_len; i++) {
-        fmt_name[i] = ' ';
+    if (ftwbuf->level == 0) {
+        printf("%s\n", name);
+        free((char *) name);
+        free((char *) last_dir);
+        return 0;
     }
 
-    int fmt_index = (fmt_name_len - name_len) - 3;
-    strcpy(fmt_name+fmt_index, fmt_tok);
-    strcpy(fmt_name+(indentation * 4), name);
+    size_t name_len = strlen(name);
+    size_t indent = ftwbuf->level * 4;
+    size_t fmt_name_len = name_len + indent;
+    size_t fmt_tok_len = strlen(FMT_TOK);
+    size_t fmt_offset = fmt_name_len - name_len - fmt_tok_len;
+
+    char *fmt_name = calloc(fmt_name_len + 1, sizeof(char));
+
+    if (!fmt_name) {
+        perror("psm: allocation error\n");
+        free((char *) name);
+        free((char *) last_dir);
+        return -1;
+    }
+
+    memset(fmt_name, ' ', fmt_name_len);
+
+    strcpy(fmt_name+fmt_offset, FMT_TOK);
+    strcpy(fmt_name+indent, name);
 
     printf("%s\n", fmt_name);
-    
+
     free(fmt_name);
+    free((char *) name);
+    free((char *) last_dir);
     return 0;
 }
 
-char *remove_ext(char *fname, char *ext) 
-{
-    int ext_len = strlen(ext);
-    int fname_len = strlen(fname);
-    int no_ext_len = fname_len - ext_len;
-
-    char *no_ext_fname = malloc(sizeof(char) * (no_ext_len + 1));
-
-    if (check_allocation(no_ext_fname)) return NULL;
-
-    if (strcmp(fname+no_ext_len, ext) == 0) {
-        strcpy(no_ext_fname, fname);
-        no_ext_fname[no_ext_len] = '\0';
-        return no_ext_fname;
-    }
-
-    return fname;
-}
-
 /*
-    This function returns the last token of a path
+    This function returns the last token of a path.
 
     Example: /foo/boo -> boo
 */
-char *get_last_dir(char *path) 
+char *get_last_dir(const char *path) 
 {
-    if (path[0] == '/') {
-
-        int path_len = strlen(path);
-        int first_index = 1;
-
-        for (int i = path_len; (i >= 1 && first_index == 1); i--) {
+    size_t path_len = strlen(path);
+    char *new_path = calloc(path_len + 1, sizeof(char));
+    
+    if (strchr(path, '/')) {
+    
+        int last_index = 1;
+        
+        for (int i = (path_len - 1); (i >= 1 && last_index == 1); i--) {
             if (path[i] == '/') {
-                first_index = (i + 1);
+                last_index = i;
             }
         }
 
-        char *new_path = malloc(sizeof(char) * (path_len - first_index + 1));
+        new_path = realloc(new_path, sizeof(char) * ((path_len - 1) - last_index + 1));
 
-        strcpy(new_path, path+first_index);
-        return new_path;
+        strcpy(new_path, path+(last_index + 1));
+    } else {
+        strcpy(new_path, path);
     }
 
-    return path;
-}
-
-/*
-    This function checks if the path is a
-    directory
-*/
-int is_dir(char *path) 
-{
-    struct stat fstat;
-
-    if (stat(path, &fstat) < 0) {
-        return -1;
-    }
-
-    if (S_ISDIR(fstat.st_mode)) {
-        return 0;
-    }
-
-    return -1;
-}
-
-/*
-    This function checks if the path is a
-    file
-*/
-int is_file(char *path) 
-{
-    struct stat fstat;
-
-    if (stat(path, &fstat) < 0) {
-        return -1;
-    }
-
-    if (S_ISREG(fstat.st_mode)) {
-        return 0;
-    }
-    
-    return -1;
+    return new_path;
 }
 
 /*
@@ -344,16 +123,43 @@ int is_file(char *path)
         
         root/{rel_path}
 */
-char *build_path(char *root, char *rel_path) 
+int build_path(char **root, char *rel_path) 
 {
-    size_t root_size = strlen(root);
+    size_t root_size = strlen(*root);
     size_t rel_path_size = strlen(rel_path);
 
-    char *full_path = malloc(sizeof(char) * root_size + 1 + rel_path_size + 1);     // {root}/{rel_path}
+    *root = realloc(*root, sizeof(char) * (root_size + 1 + rel_path_size + 1));     // {root}/{rel_path}
 
-    strcpy(full_path, root);
-    strcat(full_path, "/");
-    strcat(full_path, rel_path);
+    if (!*root) {
+        perror("psm: allocation error\n");
+        return -1;
+    }
 
-    return full_path;
+    strcat(*root, "/");
+    strcat(*root, rel_path);
+
+    return 0;
+}
+
+char *remove_ext(const char *name, const char *ext) 
+{
+    int ext_len = strlen(ext);
+    int name_len = strlen(name);
+    int no_ext_len = name_len - ext_len;
+
+    char *no_ext_name;
+
+    if ((no_ext_len >= 0) && (strcmp(name+no_ext_len, ext) == 0)) {
+        
+        no_ext_name = calloc(no_ext_len + 1, sizeof(char));
+        if (check_allocation(no_ext_name)) return NULL; 
+
+        memset((char *) name+no_ext_len, '\0', ext_len);
+        strcpy(no_ext_name, name);
+    } else {
+        no_ext_name = calloc(name_len + 1, sizeof(char));
+        strcpy(no_ext_name, name);
+    }
+
+    return no_ext_name;
 }
