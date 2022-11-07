5cd7850bf601ed1ad8af83b4886de8341319d8bc
some0necoding <codingandprogramming.code@gmail.com>
2022-11-07 23:03:46 +0100

    finished add command; testing needed

--- commands/source/psm_add.c
+++ commands/source/psm_add.c
@@ -7,19 +7,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*----------CONSTANTS-DEFINITION-START----------*/  
-
-/*-----------CONSTANTS-DEFINITION-END-----------*/
-
 /*----------FUNCTIONS-DEFINITION-START----------*/
 
 int build_path(char **root, char *rel_path); 
+char *add_ext(char *fname, const char *ext);
 
 /*-----------FUNCTIONS-DEFINITION-END-----------*/
-
-/*------------GLOBAL-VARIABLES-START------------*/
-
-/*-------------GLOBAL-VARIABLES-END-------------*/
 
 /*
     This function adds a new file to PATH encrypting it using
@@ -34,77 +27,81 @@ int build_path(char **root, char *rel_path);
 */
 int psm_add(char **args)
 {
-    /*
-        Syntax:
-            add [FILE] 
-        
-        What the fuck do we have to do?
-
-            1. check that args[1] exists
-            2. check args[1] validity
-            3. ask for password using secure input
-            4. encrypt password with gpg public key which id will be stored in/etc/pwman.con
-            5. create file at FILE location
-            6. write encrypted password to file
-    */
-
-    char *password_plain = calloc(1, sizeof(char));
-    char *path = get_env_var("PATH");
+    char *plaintext = calloc(1, sizeof(char));
+    char *rel_path = NULL;
+    
+    const char *PATH = get_env_var("PATH");
+    const char *GPG_ID = get_env_var("GPG_ID"); 
+    const char *cyphertext = NULL;
 
     size_t wlen;
     size_t last_index;
 
-    if (!password_plain) {
-        perror("psm: allocation error\n");
-        return -1;
+    int ret_code = -1;
+
+    if (!plaintext || !PATH || !GPG_ID) {
+        perror("psm: allocation error");
+        goto ret;
     }
 
     if (!args[1]) {
         printf("You missed an argument\n"
                         "\tSyntax: add [FILE]\n");
-        return -1;
+        goto ret;
     }
 
     last_index = strlen(args[1]) - 1;
 
     if (args[1][last_index] == '/') {
         printf("Cannot create empty directories\n");
-        return -1;
+        goto ret;
+    }
+    
+    rel_path = add_ext(args[1], ".gpg"); 
+
+    if (!rel_path) {
+        perror("psm: allocation error");
+        goto ret;
     }
 
-    printf("Insert password for %s: ", args[1]);
+    printf("Insert password for %s: ", rel_path);
 
-    if (read_line_s(&password_plain, 1) != 0) {
-        perror("psm: allocation error\n");
-        return -1;
+    if (read_line_s(&plaintext, 1) != 0) {
+        perror("psm: allocation error");
+        goto ret;
     } 
 
-    if (build_path(&path, args[1]) != 0) {
-        perror("psm: allocation error\n");
-        return -1;
+    if (build_path((char **) &PATH, rel_path) != 0) {
+        perror("psm: allocation error");
+        goto ret;
     } 
 
-    const char *GPG_ID = get_env_var("GPG_ID"); 
-    const char *password_enc;
-
-    if (!(password_enc = gpg_encrypt(password_plain, GPG_ID))) {
-        perror("psm: cryptography error\n");
-        return -1;
+    if (!(cyphertext = gpg_encrypt(plaintext, GPG_ID))) {
+        perror("psm: allocation error");
+        goto ret;
     }
 
-    FILE *file = fopen(path, "w");
+    FILE *file = fopen(PATH, "w");
 
     if (!file) {
         perror("psm: I/O error");
-        return -1;
+        goto ret;
     }
 
-    if ((wlen = fwrite(password_enc, sizeof(char), strlen(password_enc), file)) < 0) {
+    if ((wlen = fwrite(cyphertext, sizeof(char), strlen(cyphertext), file)) < 0) {
         perror("psm: I/O error");
-        return -1;
+        goto ret; 
     }
 
-    return 0;
+    ret_code = 0;
+
+ret:
+    rel_path ? free(rel_path) : 0;
+    GPG_ID ? free((char *) GPG_ID) : 0;
+    file ? fclose(file) : 0;
+    PATH ? free((char *) PATH) : 0;
+    plaintext ? free(plaintext) : 0;
+    return ret_code;
 }
 
 /*
@@ -128,4 +125,26 @@ int build_path(char **root, char *rel_path)
     strcat(*root, rel_path);
 
     return 0;
+}
+
+/*
+    This function adds the extension ext to
+    a string fname.
+*/
+char *add_ext(char *fname, const char *ext)
+{
+    size_t ext_size = strlen(ext);
+    size_t fname_size = strlen(fname);
+
+    char *new_fname = calloc(fname_size + ext_size + 1, sizeof(char));
+
+    if (!new_fname) {
+        perror("psm: allocation error");
+        return NULL;
+    }
+
+    strcpy(new_fname, fname);
+    strcat(new_fname, ext);
+
+    return new_fname;
 }
