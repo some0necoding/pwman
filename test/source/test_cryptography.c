44b7d4b34530a3caae76cf58f550fe101082512c
some0necoding <codingandprogramming.code@gmail.com>
2022-11-07 22:09:46 +0100

    testing

--- test/source/test_cryptography.c
+++ test/source/test_cryptography.c
@@ -5,18 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) 
+{
+    const char *GPG_ID = "2FDC14FAD4542E966B17C8F2558093477E4CB481";
+    const char *cyphertext = gpg_encrypt("ciao", GPG_ID); 
 
-    const char *text = gpg_encrypt("ciao", "2FDC14FAD4542E966B17C8F2558093477E4CB481");
-    int ch;
-
-    if (text) {
-        printf("%s\n", text);
-    } else {
+    if (!cyphertext) {
         perror("test: allocation error");
         return -1;
     }
 
-    free((char *) text);
+    printf("test: cyphertext: %s\n", cyphertext);
+
+    const char *plaintext = gpg_decrypt((char *)cyphertext, GPG_ID);
+
+    if (!plaintext) {
+        perror("test: allocation error");
+        return -1;
+    }
+
+    printf("test: plaintext: %s\n", plaintext);
+
     return 0;
 }
