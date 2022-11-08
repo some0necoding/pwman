ef59e8e94b0487e738eb80351e01c03223898cd6
some0necoding <codingandprogramming.code@gmail.com>
2022-11-08 22:44:04 +0100

    deleting unused functions, commenting and refactoring fio util code

--- utils/headers/fio.h
+++ utils/headers/fio.h
@@ -4,26 +4,13 @@
 #include <stddef.h>
 #include <stdio.h>
 
-// returns the size of a null-terminated file.
-size_t fsize(char *file_path);
+/* Returns the size of a stream */
+size_t fsize(char *path);
 
-// securely stores all bytes comprised between the beginning of the file and EOF into
-// "ret_buff" (that needs to be allocated using sodium_malloc).
-int fgetalls(char *file_path, char **ret_buff, size_t ret_buff_size);
+/* Returns all bytes contained in a stream */
+int fgetall(char *path, char **buf, size_t bufsize);
 
-// stores all bytes comprised between the beginning of the file and EOF in a secure way 
-// into "ret_buff".
-int fgetall(char *file_path, char **ret_buff, size_t ret_buff_size);
-
-// securely stores all bytes comprised between start_pos (inclusive) and EOF into 
-// "ret_buff" (that needs to be allocated using sodium_malloc).
-int fgetfroms(char *file_path, int start_pos, char **ret_buff, size_t ret_buff_size);
-
-// securely stores all bytes comprised between start_pos (inclusive) and end_pos (exclusive) 
-// into "ret_buff" (that needs to be allocated using sodium_malloc).
-int fgetfromtos(char *file_path, int start_pos, int end_pos, char **ret_buff, size_t ret_buff_size);
-
-// it reads a single line from the file stored at file_path
+/* Reads a single line from file */
 int freadline(FILE *file, char **ret_buff, size_t bufsize);
 
 #endif
--- utils/source/fio.c
+++ utils/source/fio.c
@@ -1,338 +1,145 @@
-#include "../headers/stdioplusplus.h"
+#include "../headers/fio.h"
 
-#include "../headers/sodiumplusplus.h"
-#include "../headers/array_handling.h"
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #define BUFSIZE 64
 
-// returns the size of a null-terminated stream (thus this will not work 
-// with bytes streams that contains multiple null bytes or does not contain 
-// it at all).
-size_t fsize(char *file_path)
+/*
+    This function returns the size of a 
+    null-terminated stream.
+*/
+size_t fsize(char *path)
 {
-    FILE *file = fopen(file_path, "r");
+    FILE *file = fopen(path, "r");
     size_t size = 0;
-    
+    int ret_code = -1;
+
     if (!file) {
-        perror("psm: allocation error");
-        return -1;
+        perror("psm: I/O error");
+        goto ret;
     }
 
-    // the cursor is positioned at the end of the stream
+    /* Move cursor to end of stream */
     if (fseek(file, 0, SEEK_END) != 0) {
         perror("psm: I/O error");
-        fclose(file);
-        return -1;
-    }
-
-    // file size is now equal to the cursor position
-    if((size = (size_t) ftell(file)) == -1) {
-        perror("psm: I/O error");
-        fclose(file);
-        return -1;
-    }
-
-    fclose(file);
-    return size;
-}
-
-// stores all stream bytes comprised between the beginning of the stream and the first null 
-// byte of the stream into "ret_buff" (that needs to be allocated using sodium_malloc). All 
-// buffers are allocated using sodium_malloc (doc.libsodium.org).
-int fgetalls(char *file_path, char **ret_buff, size_t ret_buff_size)
-{
-    size_t tmp_size;
-    size_t rlen;                                // total bytes returned by fread
-    size_t file_size;
-
-    FILE *file = fopen(file_path, "rb");
-
-    int ret_code = -1;                     
-
-    // checking for good opening
-    if (!file) {
-        perror("psm: allocation error");
-        return -1;
-    }
-
-    // getting the file size
-    if ((file_size = fsize(file_path)) == -1) {
-        perror("error while seeking for file size");
         goto ret;
     }
 
-    tmp_size = file_size + 1;
-
-    // creating a buffer of the same size as the file (+ '\0') to hold its content
-    if (ret_buff_size < tmp_size) {
-        *ret_buff = (char *) sodium_realloc(*ret_buff, ret_buff_size, tmp_size);
-
-        if (!*ret_buff) {
-            perror("psm: allocation error");
-            goto ret;
-        }
-    }
-
-    // reading as many bytes as the file size
-    rlen = fread(*ret_buff, 1, file_size, file);
-
-    // error checking
-    if ((rlen != file_size) && (ferror(file) != 0)) {
+    /* File size equals cursor position */ 
+    if((size = (size_t) ftell(file)) < 0) {
         perror("psm: I/O error");
         goto ret;
     }
 
-    // null-terminating the buffer and closing the file
-    memcpy(*ret_buff+file_size, "\0", 1);
-    
-    ret_code = 0;
+    ret_code = size;
 
 ret:
-    fclose(file);
+    file ? fclose(file) : 0;
     return ret_code;
 }
 
-// stores all stream bytes comprised between the beginning of the stream
-// and the first null byte of the stream into "ret_buff".
-int fgetall(char *file_path, char **ret_buff, size_t ret_buff_size)
+/*
+    This function returns all bytes of a stream
+*/
+int fgetall(char *path, char **buf, size_t bufsize)
 {
-    size_t tmp_size;
-    size_t rlen;                                // total bytes returned by fread
+    FILE *file = fopen(path, "rb");
+    
+    size_t rlen;
     size_t file_size;
-
-    FILE *file = fopen(file_path, "rb");
 
     int ret_code = -1;
 
-    // checking for good opening
     if (!file) {
         perror("psm: allocation error");
-        return -1;
+        goto ret; 
     }
 
-    // getting the file size
-    if ((file_size = fsize(file_path)) == -1) {
-        perror("psm: error while seeking for file size");
+    if ((file_size = fsize(path)) == -1) {
+        perror("psm: I/O error");
         goto ret;
     }
 
-    tmp_size = file_size + 1;
+    /* Create buffer */
+    if (bufsize < (file_size + 1)) {
+        *buf = realloc(*buf, sizeof(char *) * (file_size + 1));
 
-    // creating a buffer of the same size as the file (+ '\0') to hold its content
-    if (ret_buff_size < tmp_size) {
-        *ret_buff = (char *) realloc(*ret_buff, tmp_size);
-
-        if (!*ret_buff) {
+        if (!*buf) {
             perror("psm: allocation error");
             goto ret;
         }
     }
 
-    // reading as many bytes as the file size
-    rlen = fread(*ret_buff, 1, file_size, file);
+    rlen = fread(*buf, 1, file_size, file);
 
-    // error checking
+    /* Error checking */
     if ((rlen != file_size) && (ferror(file) != 0)) {
         perror("psm: I/O error");
         goto ret;
     }
 
-    // null-terminating the buffer and closing the file
-    memcpy(*ret_buff+file_size, "\0", 1);
+    memcpy(*buf+file_size, "\0", 1);
 
     ret_code = 0;
 
 ret:    
-    fclose(file);
+    file ? fclose(file) : 0;
     return ret_code;
 }
 
-// this function gets the string of bytes contained between start_pos (inclusive) 
-// and the first null-terminated byte of a stream, storing it in "ret_buff" (that
-// needs to be allocated using sodium_malloc). It uses only sodium-allocated buffers.
-int fgetfroms(char *file_path, int start_pos, char **ret_buff, size_t ret_buff_size)
+/*
+    This function reads a line from file and can be 
+    used to read a file line by line. It returns the
+    number of characters read on success, 0 on EOF 
+    reached and -1 on failure.
+*/
+int freadline(FILE *file, char **buf, size_t bufsize) 
 {
-    size_t tmp_size;
-    size_t file_size;
-    size_t content_len;                         // number of bytes between start_pos and EOF
-    size_t rlen;                                // total bytes returned by fread
-
-    FILE *file = fopen(file_path, "rb");
-
-    int ret_code = -1;
-
-    // checking for good opening
-    if (!file) {
-        perror("psm: allocation error");
-        return -1;
-    }
-
-    // getting file size
-    if ((file_size = fsize(file_path)) == -1) {
-        perror("psm: error while seeking for file size");
-        goto ret;
-    }
-
-    // input checking
-    if (start_pos > file_size | start_pos < 0) {
-        perror("psm: invalid stream position");
-        goto ret;
-    }
-
-    // creating a buffer to hold content from start_pos to EOF
-    content_len = file_size - start_pos;
-    tmp_size = content_len + 1;
-
-    if (ret_buff_size < tmp_size) {
-        *ret_buff = (char *) sodium_realloc(*ret_buff, ret_buff_size, tmp_size);
-
-        if (!*ret_buff) {
-            perror("psm: allocation error");
-            goto ret;
-        }
-    }
-
-
-    // moving the stream position indicator to start_pos
-    if (fseek(file, start_pos, SEEK_SET) != 0) {
-        perror("psm: I/O error");
-        goto ret;
-    }
-
-    // reading as many bytes as contained between start_pos and EOF
-    rlen = fread(*ret_buff, 1, content_len, file);
-
-    // error checking
-    if ((rlen != content_len) && (ferror(file) != 0)) {
-        perror("psm: I/O error");
-        goto ret;
-    }
-
-    // null-terminating the buffer and closing the file
-    memcpy(*ret_buff+content_len, "\0", 1);
-
-    ret_code = 0;
-
-ret:
-    fclose(file);
-    return ret_code;
-}
-
-// this function gets the file content from start_pos (inclusive) to end_pos (exclusive) and
-// stores it in "ret_buff" (that should be allocated using sodium_malloc). 
-// It uses only sodium-allocated buffers. The stream does not either need to be null-terminated
-// or to cointain only one null byte.
-int fgetfromtos(char *file_path, int start_pos, int end_pos, char **ret_buff, size_t ret_buff_size)
-{
-    size_t tmp_size;
-    size_t file_size;
-    size_t content_len;                         // number of bytes between start_pos and end_pos
-    size_t rlen;                                // total bytes returned by fread
-
-    FILE *file = fopen(file_path, "rb");
-
-    int ret_code = -1;
-
-    // checking for good opening
-    if (!file) {
-        perror("psm: allocation error");
-        return -1;
-    }
-
-    if ((file_size = fsize(file_path)) == -1) {
-        perror("psm: error while seeking for file size");
-        goto ret;
-    }
-
-    // input checking
-    if (start_pos < 0 | end_pos < 0 | end_pos < start_pos | end_pos > file_size | start_pos > file_size) {
-        perror("psm: invalid stream position");
-        goto ret;
-    }
-
-    // creating a buffer to hold content from start_pos to end_pos
-    content_len = end_pos - start_pos;                                    // selected content + \0 
-    tmp_size = content_len + 1;
-
-    if (ret_buff_size < tmp_size) {
-        *ret_buff = (unsigned char *) sodium_realloc(*ret_buff, ret_buff_size, tmp_size);
-
-        if (!*ret_buff) {
-            perror("psm: allocation error");
-            goto ret;
-        }
-    }
-
-    // moving the stream position indicator to start_pos
-    if (fseek(file, start_pos, SEEK_SET) != 0) {
-        perror("psm: I/O error");
-        goto ret;
-    }
-
-    // reading as many bytes as contained between start_pos and EOF
-    rlen = fread(*ret_buff, 1, content_len, file);
-
-    // error checking
-    if ((rlen != content_len) && (ferror(file) != 0)) {
-        perror("psm: I/O error");
-        goto ret;
-    }
-
-    // null-terminating the buffer and closing the file
-    memcpy(*ret_buff+content_len, "\0", 1);
-
-    ret_code = 0;
-
-ret:
-    fclose(file);
-    return ret_code;
-}
-
-// it reads a line from file and can be used to read a file line
-// by line. When it reaches EOF it returns EOF (-1), -2 is used
-// for errors.
-int freadline(FILE *file, char **ret_buff, size_t bufsize) 
-{
-    char c;
+    char ch;
     int pos = 0;
 
     if (!file) {
         perror("psm: I/O error");
-        return -2;
+        return -1;
     }
 
-    while ((c = fgetc(file)) != EOF && c != '\n') {
-        memcpy(*ret_buff+pos, &c, sizeof(char));
+    while ((ch = fgetc(file)) != EOF && ch != '\n') {
+        
+        memcpy(*buf+pos, &ch, sizeof(char));
         pos++;
 
+        /* Stretch buffer if needed */
         if (pos >= bufsize) {
 
-            size_t old_size = bufsize;
             bufsize += BUFSIZE;
-            *ret_buff = realloc(*ret_buff, sizeof(**ret_buff) * bufsize);
+            *buf = realloc(*buf, sizeof(char *) * bufsize);
 
-            if (!*ret_buff) {
+            if (!*buf) {
                 perror("psm: allocation error");
-                return -2;
+                return -1;
             }
-
-            // set to null newly allocated memory in order
-            // to prevent undefined behaviour
-            memset(*ret_buff+old_size, '\0', (BUFSIZE - 1));
         }
     }
 
+    /* Stretch buffer to fit NULL char if needed */
     if (pos >= bufsize) {
+        
         bufsize += 1;
-        *ret_buff = realloc(*ret_buff, sizeof(**ret_buff) * bufsize);
-        memcpy(*ret_buff+pos, '\0', sizeof(char));
-    }
+        *buf = realloc(*buf, sizeof(**buf) * bufsize);
 
-    if (c == EOF) {
-        return EOF;
+        if (!*buf) {
+            perror("psm: allocation error");
+            return -1;
+        }
+    }
+        
+    memcpy(*buf+pos, "\0", sizeof(char));
+
+    /* If EOF is reached return 0 */
+    if (ch == EOF) {
+        return 0;
     }
 
     return pos;
