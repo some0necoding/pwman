3488e0082ad7678f537dfb441f09cdc8809f3cd5
some0necoding <codingandprogramming.code@gmail.com>
2022-11-08 23:44:01 +0100

    show command fixed, commented and refactored

--- commands/source/psm_show.c
+++ commands/source/psm_show.c
@@ -7,13 +7,14 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <ftw.h>
+#include <errno.h>
 
 /*----------FUNCTIONS-DEFINITION-START----------*/
 
 int build_path(char **root, char *rel_path);
+int remove_ext(char **fname, const char *ext); 
 int print_file(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
 char *get_last_dir(const char *path);
-char *remove_ext(const char *fname, const char *ext); 
 
 /*-----------FUNCTIONS-DEFINITION-END-----------*/
 
@@ -26,25 +27,35 @@ char *remove_ext(const char *fname, const char *ext);
 */ 
 int psm_show(char **args)
 {
-    char *path = get_env_var("PATH");                                   // /home/{user}/.pwstore     
+    char *PATH = get_env_var("PATH");
+    int ret_code = -1;
 
-    if (!path) {
+    if (!PATH) {
         perror("psm: allocation error\n");
-        return -1;
+        goto ret;
     }    
 
-    if (args[1] && (build_path(&path, args[1]) != 0)) {      // /home/{user}/.pwstore/{args[1]}
+    if (args[1] && (build_path(&PATH, args[1]) != 0)) {
         perror("psm: allocation error");
-        return -1;
+        goto ret;
     }
 
-    if (nftw(path, print_file, 20, 0) == -1) {
-        perror("psm: I/O error");
-        return -1;
+    /* Recursively call print_file on all directories and files */
+    if (nftw(PATH, print_file, 20, 0) == -1) {
+        if (errno == 2) {
+            printf("No such file or directory\n");
+        } else {
+            perror("psm: I/O error");
+        }
+
+        goto ret;
     }
 
-    free(path);
-    return 0;
+    ret_code = 0;
+
+ret:
+    PATH ? free(PATH) : 0;
+    return ret_code;
 }
 
 /*
@@ -52,41 +63,51 @@ int psm_show(char **args)
 */
 int print_file(const char *path, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
-    const char *last_dir = get_last_dir(path);
-    const char *name = remove_ext(last_dir, ".gpg");
     const char *FMT_TOK = "|--";
+    char *last_dir = get_last_dir(path);
 
+    int ret_code = -1;
+
+    if (!last_dir) {
+        perror("psm: allocation error");
+        goto ret;
+    }
+    
+    remove_ext(&last_dir, ".gpg");
+
+    /* If indentation level equals 0 */ 
     if (ftwbuf->level == 0) {
-        printf("%s\n", name);
-        free((char *) name);
-        free((char *) last_dir);
-        return 0;
+        printf("%s\n", last_dir);
+        ret_code = 0;
+        goto ret;
     }
 
+    /* Every indentation level equals 4 spaces */
     size_t indent = ftwbuf->level * 4;
-    size_t fmt_name_len = strlen(name) + indent;
+    size_t fmt_name_len = strlen(last_dir) + indent;
     size_t fmt_offset = indent - strlen(FMT_TOK);
 
     char *fmt_name = calloc(fmt_name_len + 1, sizeof(char));
 
     if (!fmt_name) {
         perror("psm: allocation error\n");
-        free((char *) name);
-        free((char *) last_dir);
-        return -1;
+        goto ret; 
     }
 
+    /* Set all string to whitespaces */
     memset(fmt_name, ' ', fmt_name_len);
 
     strcpy(fmt_name+fmt_offset, FMT_TOK);
-    strcpy(fmt_name+indent, name);
+    strcpy(fmt_name+indent, last_dir);
 
     printf("%s\n", fmt_name);
 
-    free(fmt_name);
-    free((char *) name);
-    free((char *) last_dir);
-    return 0;
+    ret_code = 0;
+
+ret:
+    fmt_name ? free(fmt_name) : 0;
+    last_dir ? free((char *) last_dir) : 0;
+    return ret_code;
 }
 
 /*
@@ -98,18 +119,20 @@ char *get_last_dir(const char *path)
 {
     size_t path_len = strlen(path);
     char *new_path = calloc(path_len + 1, sizeof(char));
-    
+
+    /* Check if path contains at least one '/' */ 
     if (strchr(path, '/')) {
     
-        int last_index = 1;
-        
-        for (int i = (path_len - 1); (i >= 1 && last_index == 1); i--) {
+        int last_index = 0;
+
+        /* Check path backwards until a '/' is found */ 
+        for (int i = (path_len - 1); (i >= 1 && last_index == 0); i--) {
             if (path[i] == '/') {
                 last_index = i;
             }
         }
 
-        new_path = realloc(new_path, sizeof(char) * ((path_len - 1) - last_index + 1));
+        new_path = realloc(new_path, sizeof(char) * (path_len - last_index));
 
         if (!new_path) {
             perror("psm: allocation error\n");
@@ -149,41 +172,19 @@ int build_path(char **root, char *rel_path)
 
 /*
     This function returns name after removing ext suffix
-    if present
+    if present.
 
     Example: remove_ext("file_name.gpg", ".gpg") -> "file_name"
 */
-char *remove_ext(const char *name, const char *ext) 
+int remove_ext(char **name, const char *ext) 
 {
     int ext_len = strlen(ext);
-    int name_len = strlen(name);
+    int name_len = strlen(*name);
     int no_ext_len = name_len - ext_len;
 
-    char *no_ext_name = NULL;
-
-    if ((no_ext_len >= 0) && (strcmp(name+no_ext_len, ext) == 0)) {
-        
-        no_ext_name = calloc(no_ext_len + 1, sizeof(char));
-
-        if (!no_ext_name) {
-            perror("psm: allocation error\n");
-            return NULL;
-        }
-
-        memset((char *) name+no_ext_len, '\0', ext_len);
-        strcpy(no_ext_name, name);
-    
-    } else {
-        
-        no_ext_name = calloc(name_len + 1, sizeof(char));
-
-        if (!no_ext_name) {
-            perror("psm: allocation error\n");
-            return NULL;
-        } 
-        
-        strcpy(no_ext_name, name);
+    if (strcmp(*name+no_ext_len, ".gpg") == 0) {
+        name[0][no_ext_len] = '\0';
     }
 
-    return no_ext_name;
+    return 0;
 }
