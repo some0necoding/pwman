415ce706c7e24bc5f91f49ee35ea1034ec3ea2b3
some0necoding <codingandprogramming.code@gmail.com>
2022-11-08 21:47:35 +0100

    just a little refactoring

--- utils/source/termiosplusplus.c
+++ utils/source/termiosplusplus.c
@@ -8,16 +8,16 @@ struct termios disable_terminal_echo() {
     struct termios old_t;
     struct termios new_t;
 
-    (void) tcgetattr(fileno(stdin), &old_t);
+    tcgetattr(fileno(stdin), &old_t);
 
     new_t = old_t;
     new_t.c_lflag &= ~ECHO;
 
-    (void) tcsetattr(fileno(stdin), TCSAFLUSH, &new_t);
+    tcsetattr(fileno(stdin), TCSAFLUSH, &new_t);
 
     return old_t;
 }
 
 void enable_terminal_echo(struct termios old_t) {
-    (void) tcsetattr(fileno(stdin), TCSAFLUSH, &old_t);
+    tcsetattr(fileno(stdin), TCSAFLUSH, &old_t);
 }
