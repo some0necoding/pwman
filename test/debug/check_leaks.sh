989f1e40bb69ec1201856e94c4200f5d0a00c5c2
some0necoding <codingandprogramming.code@gmail.com>
2022-11-08 23:47:48 +0100

    testing

--- test/debug/check_leaks.sh
+++ test/debug/check_leaks.sh
@@ -3,8 +3,7 @@
 TEST_DIR="$HOME/coding/c/projects/pwman/test"
 
 valgrind \
---leak-check=yes \
---track-origins=yes \
---log-file=$TEST_DIR/debug/valgrind.log \
-$TEST_DIR/bin/test.o > $TEST_DIR/debug/valgrind_output.txt
-#--show-leak-kinds=all \
+    --leak-check=yes \
+    --track-origins=yes \
+    --log-file=$TEST_DIR/debug/valgrind.log \
+    $TEST_DIR/bin/test.o > $TEST_DIR/debug/valgrind_output.txt
