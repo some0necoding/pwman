46f26d2c09cac09bc744bc55739a91614fa746c1
some0necoding <codingandprogramming.code@gmail.com>
2022-11-11 23:23:23 +0100

    defining CONFIGS macro

--- utils/headers/config.h
+++ utils/headers/config.h
@@ -1,6 +1,8 @@
 #ifndef ENVIRONMENT_VARIABLES
 #define ENVIRONMENT_VARIABLES
 
+#define CONFIGS "/etc/pwman.conf"
+
 char *get_config_path();
 int add_env_var(char *key, char *value);
 char *get_env_var(char *key);
