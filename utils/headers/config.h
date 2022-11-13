52b66a3badccaa01c7bef20f25811bd05e1995bb
some0necoding <codingandprogramming.code@gmail.com>
2022-11-13 13:16:35 +0100

    changing config file path

--- utils/headers/config.h
+++ utils/headers/config.h
@@ -1,7 +1,7 @@
 #ifndef ENVIRONMENT_VARIABLES
 #define ENVIRONMENT_VARIABLES
 
-#define CONFIG_FILE "/etc/pwman.conf"
+#define CONFIG_FILE "/home/marco/.config/pwman.conf"
 
 char *get_config_path();
 int add_env_var(char *key, char *value);
