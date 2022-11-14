8ef10e4d7ecdc8379a0e3697b7666deb0690508b
some0necoding <codingandprogramming.code@gmail.com>
2022-11-14 14:01:58 +0100

    fixed help command

--- commands/source/psm_help.c
+++ commands/source/psm_help.c
@@ -2,47 +2,25 @@
 
 int psm_help(char **args)
 {
-    unsigned char *help_txt;
+    char *help_txt;
 
     if (args[1]) {
         printf("\"help\" does not accept arguments\n");
         return -1;
     }
 
-    help_txt = "\nWHAT IS PASSMAN:\n"
-               "\n"
-               "    Passman stores and retrieves accounts along with\n"
-	           "    their passwords.\n"
-               "\n"
-	           "    Every account is made up by three parts:\n"
-		       "        - account name\n" 
-		       "        - email or username used to log in the account\n"
-		       "        - password used to log in the account\n"
+    help_txt = "\n"
+               "    Pwman is a really simple password manager\n"
                "\n"
                "SYNTAX:\n" 
-               "\n"
-	           "    show\n" 
-	           "    add <account-name> <user-or-mail>\n" 
-	           "    rm <account-name>\n"
-	           "    get <account-name>\n" 
-	           "    help\n"
-               "\n"
-               "DESCRIPTION:\n"
-               "\n"
-	           "    show\n" 
-		       "        To show all accounts stored.\n"
-               "\n"
-	           "    add\n" 
-		       "        To add a new account.\n"
-               "\n"
-	           "    rm\n"
-		       "        To remove an account.\n" 
-               "\n"
-	           "    get\n"
-		       "        To retrieve a password. Note that it will be saved\n"
-               "        into the (X11) clipboard so that it can be directly\n"
-               "        pasted.\n"
-               "\n"
+	           "    show [FILE]\n" 
+		       "        Show all stored files.\n"
+	           "    add FILE\n" 
+		       "        Add a new file.\n"
+	           "    rm FILE\n"
+		       "        Remove a file.\n" 
+	           "    get FILE\n"
+		       "        Retrieve a password and stores it into the clipboard.\n"
 	           "    help\n" 
 		       "        To display help page.\n";
 
