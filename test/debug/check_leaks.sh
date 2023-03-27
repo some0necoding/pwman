d35c0d3838222e68a35798feb94510ba4b0aa631
some0necoding <codingandprogramming.code@gmail.com>
2023-03-27 20:20:16 +0200

    removing ignored files

--- test/debug/check_leaks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TEST_DIR="$HOME/coding/c/projects/pwman/test"
-
-valgrind \
-    --leak-check=yes \
-    --track-origins=yes \
-    --log-file=$TEST_DIR/debug/valgrind.log \
-    $TEST_DIR/bin/test.o > $TEST_DIR/debug/valgrind_output.txt
